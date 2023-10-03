81a078096915ed6374bc762a30633bdf6f4e3ecc
Filomn Abrha <filmonabrha2020@gmail.com>
2023-10-03 10:24:10 +0300

    Create a mobile phone breakpoint

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 const navigation = () => {
     const hamburgerMenu = document.querySelector("header button");
     const nav = document.querySelector("nav");
-    const close = document.querySelector("button.close");
 
     hamburgerMenu.addEventListener("click", (event) => {
-        // nav.className = "show-nav";
         nav.classList.add("show-nav");
-        // close.style.display = "inline-flex";
-        // event.target.style.display = "none";
     });
-    close.addEventListener("click", () => {
+    nav.addEventListener("click", () => {
         nav.classList.remove("show-nav");
     });
 };
